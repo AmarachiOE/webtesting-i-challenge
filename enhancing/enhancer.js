a1e08c2f12604f2d76bc97b746073548882ea653
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-13 12:41:08 -0700

    Test for succeed() method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,8 +5,24 @@ module.exports = {
   get,
 };
 
+/* 
+
+When enhancement succeeds
+-- The item's enhancement increases by 1.
+-- If the item enhancement level is 20, the enhancement level is not changed.
+-- The durability of the item is not changed.
+
+*/
+
 function succeed(item) {
-  return { ...item };
+
+  const toEnhance = (item.enhancement == 20) ? item.enhancement : item.enhancement+1;
+
+  return { 
+    ...item,
+  enhancement: toEnhance
+  //(20 ? item.enhancement : item.enhancement + 1 )
+};
 }
 
 function fail(item) {
--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -8,4 +8,23 @@ describe("enhancer.js", () => {
       expect(enhancer.repair(item)).toEqual({ ...item, durability: 100 });
     });
   });
+
+  describe("succeed() method", () => {
+    it("should increase item's enhancement by 1 only if originally < 20", () => {
+      const item = {
+        name: "box",
+        enhancement: 20,
+        durability: 78
+      };
+      expect(enhancer.succeed(item)).toEqual({ ...item, enhancement: 20 });
+
+      expect(
+        enhancer.succeed({
+          name: "box",
+          enhancement: 3,
+          durability: 78
+        })
+      ).toEqual({ ...item, enhancement: 4 });
+    });
+  });
 });
